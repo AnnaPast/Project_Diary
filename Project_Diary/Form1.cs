6deae9b8b15144d4cbbd36eca4e0a8389575341b
Shyzza <64161982+Shyzza@users.noreply.github.com>
2024-01-17 18:20:51 +0200

    With Json

--- Project_Diary/Form1.cs
+++ Project_Diary/Form1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Text.Json;
 using System.Windows.Forms;
 
 namespace Project_Diary
@@ -9,6 +11,9 @@ namespace Project_Diary
         public Dictionary<DateTime, DiaryProperties> diaryPropertiesDictionary = new Dictionary<DateTime, DiaryProperties>();
         public DateTime selectedDate { get; set; }
 
+        string JsonFilePath = System.IO.Path.GetDirectoryName(Application.ExecutablePath) + "/diary_properties.json"; //?????????? ?????????? ?  ??????? ????????? ??????????? ???? ???????? ??????????. 
+
+
         public class DiaryProperties
         {
             public string? Mood { get; set; }
@@ -18,6 +23,9 @@ namespace Project_Diary
 
         public Form1()
         {
+            Debug.WriteLine("JsonFile path: " + JsonFilePath);
+            ReadDictionaryFromFile(JsonFilePath);
+
             InitializeComponent();
         }
 
@@ -27,7 +35,48 @@ namespace Project_Diary
 
             Form2 secondForm = new Form2(diaryPropertiesDictionary, selectedDate);
             secondForm.ShowDialog();
+            SaveDictionaryToFile(JsonFilePath);
+            Debug.WriteLine("done");
         
+        }
+        private void ReadDictionaryFromFile(string JsonFilePathVar)
+        {
+            if (File.Exists(JsonFilePathVar))
+            {
+                string json = File.ReadAllText(JsonFilePathVar);
+
+                diaryPropertiesDictionary = JsonSerializer.Deserialize<Dictionary<DateTime, DiaryProperties>>(json);//?????????????? ?? json ? dictionart
+            }
+
+            // Print the dictionary to the debug output
+            LogoutDictionary();
+        }
+        private void LogoutDictionary()
+        {
+            Debug.WriteLine("Diary properties:");
+            foreach (var element in diaryPropertiesDictionary) //??????? ???? ????????? dictionary
+                {
+                    DateTime date = element.Key;
+                    DiaryProperties properties = element.Value;
+
+                    Debug.WriteLine($"Date: {date}. Mood: {properties.Mood}. NotesList: {properties.NotesList}. ShopingList: {properties.ShoppingList}");
+                }
+        }
+
+        private void SaveDictionaryToFile(string filePath)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions //????? ??? ??????????????????
+            {
+                WriteIndented = true
+            };
+
+            // Serialize the merged data to JSON
+            string json = JsonSerializer.Serialize(diaryPropertiesDictionary, options);
+
+            // Write the merged data to the file
+            File.WriteAllText(filePath, json);
+            LogoutDictionary();
+
         }
 
         private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
--- Project_Diary/obj/Debug/net8.0-windows/Project_Diary.AssemblyInfo.cs
+++ Project_Diary/obj/Debug/net8.0-windows/Project_Diary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Diary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0936b4a32c5e93541fb42f954be543cec815b01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+796c9f8e0527a981e6af3df14d1688f468c5794b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Diary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Diary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
